4a33c42ba4a2b7495b0359921e7ef644b14de821
Mohammad Aslam <aslampaasa420@gmail.com>
2025-12-31 00:17:14 +0530

    Refactoring Blog APIs + Logic

---
+++ server/controllers/blog.controller.js
@@ -0,0 +1,129 @@
+const Blog = require("../models/blog.models");
+
+const createBlog = async (req, res) => {
+  try {
+    const { title, description, draft } = req.body;
+
+    if (!title || !description) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const blog = await Blog.create({ title, description, draft });
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog created successfully",
+      blog: blog,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error occurred while creating the blog",
+      error: error.message,
+    });
+  }
+};
+
+const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ draft: false });
+    return res.status(200).json({
+      success: true,
+      message: "Blogs fetched successfully",
+      blogs: blogs,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error occurred while fetching the blogs",
+      error: error.message,
+    });
+  }
+};
+
+const getBlogById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "Blog not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog fetched successfully",
+      blog: blog,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error occurred while fetching the blog",
+      error: error.message,
+    });
+  }
+};
+
+const updateBlogById = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, draft } = req.body;
+  try {
+    const blog = await Blog.findByIdAndUpdate(
+      id,
+      { title, description, draft },
+      { new: true }
+    );
+
+    if (!blog) {
+      return res.status(404).json({
+        success: false,
+        message: "Blog not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog updated successfully",
+      blog: blog,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error occurred while updating the blog",
+      error: error.message,
+    });
+  }
+};
+
+const deleteBlogById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const blog = await Blog.findByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "Blog deleted successfully",
+      blog: blog,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Error occurred while deleting the blog",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  updateBlogById,
+  deleteBlogById,
+};
